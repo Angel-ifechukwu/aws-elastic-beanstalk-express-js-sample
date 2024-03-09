7701197c066a9dc6002bae846ed4e0d6c45fa9fa
Angel Ifechukwu <angelifechukwu00@gmail.com>
2024-03-09 16:23:31 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Angelica, The DevOps Diva!'));
-
+app.get('/', (req, res) => res.send('Angelica, The DevOps Diva on Twitter! '));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+
